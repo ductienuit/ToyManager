67f703b4dfa332aaa974a361ad6dcd7b0d56ba25
Chung Mạnh Quỳnh <manhquynh.ms@outlook.com>
2019-05-29 14:35:02 +0700

    add functions to CategoryDAO

--- src/main/java/dao/CategoryDAO.java
+++ src/main/java/dao/CategoryDAO.java
@@ -7,6 +7,12 @@ package dao;
 
 import dao.common.BasicDAO;
 import dto.Category;
+import dto.User;
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+import utils.HibernateUtil;
+import utils.ObjectWrapper;
 
 /**
  * @author CMQ
@@ -27,13 +33,14 @@ public class CategoryDAO extends BasicDAO<Category> {
         dao.insert(c1);
         System.out.println("Success!");
 
-        // search
+        // findEntityById
         System.out.println("2. Search");
         c1.setId(1);
-        Category c2 = dao.search(c1.getId());
-        if (c2 != null)
+        Category c2 = dao.findEntityById(c1.getId());
+        if (c2 != null) {
             System.out.println("Category [id = " + c2.getId() + ", name = " + c2
-                    .getName() + "]");
+                .getName() + "]");
+        }
         System.out.println("Success!");
 
         // update
@@ -41,9 +48,9 @@ public class CategoryDAO extends BasicDAO<Category> {
         Category c3 = c2;
         c3.setName("Category 2");
         dao.update(c3);
-        Category c4 = dao.search(c3.getId());
+        Category c4 = dao.findEntityById(c3.getId());
         System.out.println("Category [id = " + c4.getId() + ", name = " + c4
-                .getName() + "]");
+            .getName() + "]");
         System.out.println("Success!");
 
         // count
@@ -61,11 +68,47 @@ public class CategoryDAO extends BasicDAO<Category> {
         // delete
         System.out.println("6. Delete");
         dao.delete(c4);
-        Category c5 = dao.search(c4.getId());
+        Category c5 = dao.findEntityById(c4.getId());
         if (c5 == null) {
             System.out.println("Attempt to find Category [id = " + c4.getId()
-                    + "]: null");
+                                   + "]: null");
         }
         System.out.println("Success!");
+    }
+
+    public Category getCategoryByCode(final String code) {
+        final ObjectWrapper<Category> categoryWrapper = new ObjectWrapper<>();
+
+        HibernateUtil.beginTransaction((session, transaction) -> {
+            Criteria criteria = session
+                .createCriteria(Category.class)
+                .add(Restrictions.eq("code",
+                                     code));
+
+            List result = criteria.list();
+            if (!result.isEmpty()) {
+                categoryWrapper.setObject((Category) result.get(0));
+            }
+        });
+
+        return categoryWrapper.getObject();
+    }
+
+    public Category getCategoryByName(final String name) {
+        final ObjectWrapper<Category> categoryWrapper = new ObjectWrapper<>();
+
+        HibernateUtil.beginTransaction((session, transaction) -> {
+            Criteria criteria = session
+                .createCriteria(Category.class)
+                .add(Restrictions.eq("name",
+                                     name));
+
+            List result = criteria.list();
+            if (!result.isEmpty()) {
+                categoryWrapper.setObject((Category) result.get(0));
+            }
+        });
+
+        return categoryWrapper.getObject();
     }
 }
